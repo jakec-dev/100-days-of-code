5dd7748b8fc97511138553d35ba523ccfd1cd3dd
Jake <jake@jakec.dev>
2022-02-28 18:22:53 +1000

    complete day 8

--- README.md
+++ README.md
@@ -13,3 +13,5 @@
 **Day 6**: [Escape the Maze (external)](https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json)
 
 **Day 7**: [Hangman](./day-007/)
+
+**Day 8**: [Caesar Cipher](./day-008/)
--- day-008/main.py
+++ day-008/main.py
@@ -1,27 +1,37 @@
+from art import logo
+
+print(logo)
+
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+def start_app():
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    if shift > len(alphabet):
+        shift = shift % len(alphabet)
+    caesar(text, shift, direction)
+    do_restart = input("Do you want to restart? [y/N] ").lower()
+    if do_restart == "y":
+        start_app()
 
 def caesar(text, shift, direction):
     output_text = ""
     for letter in text:
-        position = alphabet.index(letter)
-        if direction == "encode":
-            shifted_position = position + shift
-            if shifted_position > len(alphabet) - 1:
-                shifted_position -= len(alphabet)
+        if letter not in alphabet:
+            output_text += letter
         else:
-            shifted_position = position - shift
-            if shifted_position < 1:
-                shifted_position += len(alphabet)
-        shifted_letter = alphabet[shifted_position]
-        output_text += shifted_letter
-    if direction == "encode":
-        print_label = "encoded"
-    else:
-        print_label = "decoded"
-    print(f"The {print_label} text is {output_text}")
+            position = alphabet.index(letter)
+            if direction == "encode":
+                shifted_position = position + shift
+                if shifted_position > len(alphabet) - 1:
+                    shifted_position -= len(alphabet)
+            else:
+                shifted_position = position - shift
+                if shifted_position < 1:
+                    shifted_position += len(alphabet)
+            shifted_letter = alphabet[shifted_position]
+            output_text += shifted_letter
+    print(f"The {direction}d text is {output_text}")
 
-caesar(text, shift, direction)
+start_app()
