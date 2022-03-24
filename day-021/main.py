ab9afd9a920767551e77d856ba5e3c21d7b8fe78
Jake Clayton <jake.clayton@localsearch.com.au>
2022-03-24 14:43:42 +1000

    improve day 21 with tips from day 24

--- day-021/main.py
+++ day-021/main.py
@@ -32,12 +32,12 @@ while game_is_on:
         scoreboard.increase_score()
 
     if abs(snake.head.xcor()) > 280 or abs(snake.head.ycor()) > 280:
-        game_is_on = False
-        scoreboard.game_over()
+        scoreboard.reset()
+        snake.reset()
 
     for segment in snake.segments[1:]:
         if snake.head.distance(segment) < 10:
-            game_is_on = False
-            scoreboard.game_over()
+            scoreboard.reset()
+            snake.reset()
 
 screen.exitonclick()
