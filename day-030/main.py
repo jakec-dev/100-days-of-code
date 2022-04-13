228a56659dff408b2e5d204e1f404098d2cbba20
Jake Clayton <jake.clayton@localsearch.com.au>
2022-04-13 16:37:59 +1000

    push current progress for day 30

---
+++ day-030/main.py
@@ -0,0 +1,96 @@
+from tkinter import Canvas, Tk, PhotoImage, Label, Entry, Button, END,\
+                    messagebox
+import random
+import json
+import pyperclip
+
+# ---------------------------- PASSWORD GENERATOR -------------------------- #
+
+
+def generate_password():
+    password = entry_password.delete(0, END)
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+               'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+               'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+               'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    nr_letters = random.randint(8, 10)
+    nr_symbols = random.randint(2, 4)
+    nr_numbers = random.randint(2, 4)
+
+    password_letters = [random.choice(letters) for _ in range(nr_letters)]
+    password_symbols = [random.choice(symbols) for _ in range(nr_symbols)]
+    password_numbers = [random.choice(numbers) for _ in range(nr_numbers)]
+
+    password_list = password_letters + password_symbols + password_numbers
+    random.shuffle(password_list)
+    password = "".join(password_list)
+    entry_password.insert(0, password)
+    pyperclip.copy(password)
+
+
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+
+
+def save_password():
+    website = entry_website.get()
+    username = entry_email_uname.get()
+    password = entry_password.get()
+    new_data = {website: {
+        "email": username,
+        "password": password
+    }}
+
+    if len(website) == 0 or len(username) == 0 or len(password) == 0:
+        messagebox.showinfo(title="Oops", message="Please make sure you \
+haven't left any fields empty.")
+    else:
+        with open("./data.json", mode="r") as data_file:
+            data = json.load(data_file)
+            data.update(new_data)
+        with open("./data.json", mode="w") as data_file:
+            json.dump(data, data_file, indent=4)
+        entry_website.delete(0, END)
+        entry_password.delete(0, END)
+
+
+# ---------------------------- UI SETUP ------------------------------------ #
+
+window = Tk()
+window.title("Password Generator")
+window.config(padx=20, pady=20)
+
+canvas = Canvas(width=200, height=200)
+tomato_img = PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=tomato_img)
+canvas.grid(column=1, row=0)
+
+label_website = Label(text="Website:")
+label_website.grid(column=0, row=1)
+
+entry_website = Entry()
+entry_website.grid(column=1, row=1, columnspan=2, sticky="EW")
+entry_website.focus()
+
+label_email_uname = Label(text="Email/Username:")
+label_email_uname.grid(column=0, row=2)
+
+entry_email_uname = Entry()
+entry_email_uname.grid(column=1, row=2, columnspan=2, sticky="EW")
+entry_email_uname.insert(0, "default@example.com")
+
+label_password = Label(text="Password:")
+label_password.grid(column=0, row=3)
+
+entry_password = Entry()
+entry_password.grid(column=1, row=3, sticky="EW")
+
+generate_btn = Button(text="Generate Password", command=generate_password)
+generate_btn.grid(column=2, row=3, sticky="EW")
+
+add_btn = Button(text="Add", width=35, command=save_password)
+add_btn.grid(column=1, row=4, columnspan=2, sticky="EW")
+
+window.mainloop()
